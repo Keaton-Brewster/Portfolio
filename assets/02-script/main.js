0c51312b9f61d4b0ca463e1d4cc087b411512e52
Delta9k8b <keaton.brewster@gmail.com>
2021-02-03 09:47:57 -0600

    added portfolio information and links. Updated about me information

--- assets/02-script/main.js
+++ assets/02-script/main.js
@@ -1,77 +1,71 @@
 $(document).ready(function () {
-    const mainPage = $("[name='main-page']"), //THINK ABOUT CHANGING THESE FROM ID'S TO NAMES SO THAT MULTIPLE CAN BE SELECTED, FOR MEDIA QUERI
-        aboutmePage = $("[name='about_me-page']"),
-        myworkPage = $("[name='my_work-page']"),
-        contactPage = $("[name='contact_me-page']"),
-        pages = [mainPage, aboutmePage, myworkPage, contactPage];
+  const mainPage = $("[name='main-page']"), //THINK ABOUT CHANGING THESE FROM ID'S TO NAMES SO THAT MULTIPLE CAN BE SELECTED, FOR MEDIA QUERI
+    aboutmePage = $("[name='about_me-page']"),
+    myworkPage = $("[name='my_work-page']"),
+    contactPage = $("[name='contact_me-page']"),
+    pages = [mainPage, aboutmePage, myworkPage, contactPage];
 
-    let showMenu = false;
+  let showMenu = false;
 
+  $("body").fadeIn(3000).removeClass("hide");
 
+  $("body").on("click", "button", function (event) {
+    event.preventDefault();
 
-    $("body").fadeIn(3500).removeClass("hide");
+    //NEED TO WRITE IN A CSS QUERY TO CHANGE CLASSES AND SHIT SO THAT OTHER SCRIPT STILL WORKS
+    // WHEN THE PAGE IS RESPONSIVELY CHANGED.
 
-    $("body").on("click", "button", function (event) {
-        event.preventDefault();
+    // MAYBE NOT, IT LOOKS LIKE I JUST NEEDED TO USE .hasClass()
 
-        //NEED TO WRITE IN A CSS QUERY TO CHANGE CLASSES AND SHIT SO THAT OTHER SCRIPT STILL WORKS 
-        // WHEN THE PAGE IS RESPONSIVELY CHANGED.
+    // start by setting up the function that changes 'pages' (everything will still be built on one page, because I like that more.)
+    targetName = "[name='" + $(this).attr("name") + "-page']";
 
-        // MAYBE NOT, IT LOOKS LIKE I JUST NEEDED TO USE .hasClass()
-
-        // start by setting up the function that changes 'pages' (everything will still be built on one page, because I like that more.)
-        targetName = "[name='" + $(this).attr("name") + "-page']";
-
-        // Nothing will happen if you click the button for the 'page' you are already on. 
-        if (!$(targetName).hasClass('hide')) {
-            return;
-        }
-
-        for (let i = 0; i < pages.length; i++) {
-            if (!pages[i].hasClass('hide')) {
-                pages[i].fadeOut("slow").addClass("hide");
-            }
-        }
-        $('.menu-btn').removeClass('close');
-        showMenu = false;
-        $(targetName).fadeIn(1500).removeClass("hide");
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-    // NEED TO CHANGE THIS AROUND A LITTLE BIT. THE CSS AND HTML SEEM GOOD, BUT ON PAGE LOAD, IT DOES NOT QUITE WORK RIGHT. 
-
-    // Set the initial state of the menu button
-
-    function toggleMenu() {
-        if (!showMenu && $('#navbarDropdown').attr('aria-expanded','false')) {
-            $('.menu-btn').addClass("close");
-            // Reset the menu state 
-            showMenu = true;
-        } else {
-            $('.menu-btn').removeClass("close");
-            // Reset the menu state 
-            showMenu = false;
-        }
+    // Nothing will happen if you click the button for the 'page' you are already on.
+    if (!$(targetName).hasClass("hide")) {
+      return;
     }
 
-    $('.menu-btn').on("click", toggleMenu);
-    
-    $(document).on('click', function(event){
-        click = event.target;
+    for (let i = 0; i < pages.length; i++) {
+      if (!pages[i].hasClass("hide")) {
+        pages[i].fadeOut("slow").addClass("hide");
+      }
+    }
+    $(".menu-btn").removeClass("close");
+    showMenu = false;
+    $(targetName).fadeIn(1500).removeClass("hide");
+  });
 
-        if (!$(event.target).hasClass('menu-btn') && showMenu === true) {
-            toggleMenu();
-        }
-    })
+  $("[name='my_work-page']").on("click", ".card",function (event) {
+    event.preventDefault();
+    console.log($(this))
+    if ($(this).attr("href") != undefined) {
+      window.open($(this).attr("href"), "_blank");
+    }
+  });
 
+  // NEED TO CHANGE THIS AROUND A LITTLE BIT. THE CSS AND HTML SEEM GOOD, BUT ON PAGE LOAD, IT DOES NOT QUITE WORK RIGHT.
+
+  // Set the initial state of the menu button
+
+  function toggleMenu() {
+    if (!showMenu && $("#navbarDropdown").attr("aria-expanded", "false")) {
+      $(".menu-btn").addClass("close");
+      // Reset the menu state
+      showMenu = true;
+    } else {
+      $(".menu-btn").removeClass("close");
+      // Reset the menu state
+      showMenu = false;
+    }
+  }
+
+  $(".menu-btn").on("click", toggleMenu);
+
+  $(document).on("click", function (event) {
+    click = event.target;
+
+    if (!$(event.target).hasClass("menu-btn") && showMenu === true) {
+      toggleMenu();
+    }
+  });
 });
