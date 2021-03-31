5f024a02ff4c27bf20ed1abc7faf9cf5b231c8d1
Delta9k8b <keaton.brewster@gmail.com>
2021-03-30 19:18:32 -0500

    turning into node based web app

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+// const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+// const passport = require('./config/passport.js');
+const PORT = process.env.PORT || 1717;
+// const db = require('./models');
+
+const app = express();
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+// app.use(session({
+//   secret: 'the family whiteboard',
+//   resave: true,
+//   saveUninitialized: true
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+//set up handlebars
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//routes
+require('./controllers/api_troller')(app);
+require('./controllers/html_troller')(app);
+
+// db.sequelize.sync({force: true}).then(() => {
+try {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}.`);
+    });
+} catch (err) {
+    console.error(`Error at server.js(35): ${err}`);
+}
+// });
---
+++ public/assets/02-script/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+  const mainPage = $("[name='main-page']"), //THINK ABOUT CHANGING THESE FROM ID'S TO NAMES SO THAT MULTIPLE CAN BE SELECTED, FOR MEDIA QUERI
+    aboutMePage = $("[name='about_me-page']"),
+    myWorkPage = $("[name='my_work-page']"),
+    contactPage = $("[name='contact_me-page']"),
+    pages = [mainPage, aboutMePage, myWorkPage, contactPage];
+
+  let showMenu = false;
+
+  $("body").fadeIn(3000).removeClass("hide");
+
+  $("body").on("click", "button", function (event) {
+    event.preventDefault();
+
+    // start by setting up the function that changes 'pages' (everything will still be built on one page, because I like that more.)
+    targetName = "[name='" + $(this).attr("name") + "-page']";
+
+    // Nothing will happen if you click the button for the 'page' you are already on.
+    if (!$(targetName).hasClass("hide")) {
+      return;
+    }
+
+    for (let i = 0; i < pages.length; i++) {
+      if (!pages[i].hasClass("hide")) {
+        pages[i].fadeOut(0).addClass("hide");
+      }
+    }
+    $(".menu-btn").removeClass("close");
+    showMenu = false;
+    $(targetName).fadeIn(1500).removeClass("hide");
+  });
+
+  // NEED TO CHANGE THIS AROUND A LITTLE BIT. THE CSS AND HTML SEEM GOOD, BUT ON PAGE LOAD, IT DOES NOT QUITE WORK RIGHT.
+
+  // Set the initial state of the menu button
+
+  toggleMenu = () => {
+    if (!showMenu && $("#navbarDropdown").attr("aria-expanded", "false")) {
+      $(".menu-btn").addClass("close");
+      // Reset the menu state
+      showMenu = true;
+    } else {
+      $(".menu-btn").removeClass("close");
+      // Reset the menu state
+      showMenu = false;
+    }
+  }
+
+  $(".menu-btn").on("click", toggleMenu);
+
+  $(document).on("click", function (event) {
+    click = event.target;
+
+    if (!$(event.target).hasClass("menu-btn") && showMenu === true) {
+      toggleMenu();
+    }
+  });
+});
