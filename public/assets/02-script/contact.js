85a02230e1707a6ee596891709cdec4eb8ade9e2
Delta9k8b <keaton.brewster@gmail.com>
2021-03-31 19:22:58 -0500

    got form working FR FR this time I swear

--- public/assets/02-script/contact.js
+++ public/assets/02-script/contact.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    
+
     //todo At some point in time this should be a modal
     const thankYou = () => {
         alert('Thank you for reaching out! I will get in touch with you as soon as I can!')
@@ -21,8 +21,8 @@ $(document).ready(() => {
         const message = {
             from: 'throwawaykeatondev@gmail.com',
             to: 'keatonbrewsterdev@gmail.com',
-            subject: `${$('input[name=inquireLG_subject]').val()}, from: ${$('input[name=inquireLG_email]').val()}`,
-            text: $('textarea.inquireLG_text').val()
+            subject: $('input[name=inquireLG_subject]').val(),
+            text: `FROM: ${$('#inquireLG_email').val()}\n${$('textarea[name=inquireLG_text]').val()}`
         };
 
         sendMail(message);
@@ -32,8 +32,8 @@ $(document).ready(() => {
         const message = {
             from: 'throwawaykeatondev@gmail.com',
             to: 'keatonbrewsterdev@gmail.com',
-            subject: `${$('input[name=inquireMD_subject]').val()}, from: ${$('input[name=inquireMD_email]').val()}`,
-            text: $('textarea[name=inquireMD_text]').val()
+            subject: $('input[name=inquireMD_subject]').val(),
+            text: `${$('#inquireMD_email').val()}\n${$('textarea[name=inquireMD_text]').val()}`
         };
 
         sendMail(message);
@@ -43,8 +43,8 @@ $(document).ready(() => {
         const message = {
             from: 'throwawaykeatondev@gmail.com',
             to: 'keatonbrewsterdev@gmail.com',
-            subject: `${$('input[name=inquireSM_subject]').val()}, from: ${$('input[name=inquireSM_email]').val()}`,
-            text: $('textarea[name=inquireSM_text]').val()
+            subject: $('input[name=inquireSM_subject]').val(),
+            text: `${$('#inquireSM_email').val()}\n${$('textarea[name=inquireSM_text]').val()}`
         };
 
         sendMail(message);
